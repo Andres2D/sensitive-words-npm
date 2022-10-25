09da929be2b3763686919d1522865c012b56f48d
Andres2D <andres2d1997.1206@gmail.com>
2022-10-25 18:03:35 -0500

    Update package and add np package

--- src/index.test.js
+++ src/index.test.js
@@ -3,11 +3,11 @@ import sensitiveWords from '.';
 test('Replaces a single word with asterisks', () => {
   const message = 'The name of the NX will be egghead application';
   expect(sensitiveWords(message, ['application']))
-    .toBe('The name of the NX will be egghead ***');
+    .toBe('The name of the NX will be egghead ****');
 });
 
 test('Replaces a multiple words with asterisks', () => {
   const message = 'The name of the NX will be egghead application, The Application will be awesome';
   expect(sensitiveWords(message, ['application']))
-    .toBe('The name of the NX will be egghead ***, The *** will be awesome');
+    .toBe('The name of the NX will be egghead ****, The **** will be awesome');
 });
