6a7f1c1a996b39f90cbb432007cee5d01a40d0c7
Andres2D <andres2d1997.1206@gmail.com>
2022-10-25 17:36:41 -0500

    Add replace function with tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,13 @@
 import sensitiveWords from '.';
 
-test('Says hello world', () => {
-  expect(sensitiveWords()).toBe('Hello world');
+test('Replaces a single word with asterisks', () => {
+  const message = 'The name of the NX will be egghead application';
+  expect(sensitiveWords(message, ['application']))
+    .toBe('The name of the NX will be egghead ***');
+});
+
+test('Replaces a multiple words with asterisks', () => {
+  const message = 'The name of the NX will be egghead application, The Application will be awesome';
+  expect(sensitiveWords(message, ['application']))
+    .toBe('The name of the NX will be egghead ***, The *** will be awesome');
 });
